62b332f7a8f90626de6779585f90f18892ce989c
bhutuklearning <amritanshugoutam.dev@gmail.com>
2025-07-07 18:40:48 +0530

    Testing and correcting

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,24 @@ import dotenv from "dotenv";
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
+import path from "path";
+
 
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 10000;
+const __dirname = path.resolve();
 
 // middlewares
 app.use(express.json()); // this middleware will parse JSON bodies
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }));
+}
+
 app.use(rateLimiter);
 
 // app.use((req, res, next) => {
@@ -25,11 +32,19 @@ app.use(rateLimiter);
 //Router Endpoint is URL + HTTP
 app.use("/api/notes", noteRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 
 // home route
-app.get("/", (req, res, next) => {
-    res.send("You are at the home page.");
-})
+// app.get("/", (req, res, next) => {
+//     res.send("You are at the home page.");
+// })
 
 connectDB().then(() => {
     app.listen(PORT, (req, res) => {
